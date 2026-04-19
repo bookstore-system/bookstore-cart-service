8a07f46a6e4ec329c69a50aecf878369f10b9ab9
TruongIKPK <phitruong20192020@gmail.com>
2026-04-19 23:14:31 +0700

    first commit

---
+++ src/main/java/com/hamtech/bookstorecartservice/BookstoreCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstorecartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreCartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreCartServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamtech/bookstorecartservice/BookstoreCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstorecartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreCartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
