1664cc7af466cfdbbaa484655b3fcadc67fca9d2
TruongIKPK <phitruong20192020@gmail.com>
2026-05-31 02:04:04 +0700

    feat: add batch book request DTO and endpoint to fetch books in bulk

---
+++ src/main/java/com/notfound/cartservice/client/dto/BatchBookRequest.java
@@ -0,0 +1,16 @@
+package com.notfound.cartservice.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BatchBookRequest {
+    private List<String> ids;
+}
